834f6b63116b5335023fdcab5b9e8623614eee69
baowei <baow@chinatelecom.cn>
2020-09-14 01:37:40 +0800

    2020/9/14 建立分发模块

---
+++ coupon-service/coupon-distribution/src/main/java/com/bw/coupon/entity/Coupon.java
@@ -0,0 +1,73 @@
+package com.bw.coupon.entity;
+
+import com.bw.coupon.converter.CouponStatusEnumConverter;
+import com.bw.coupon.enumeration.CouponStatusEnum;
+import com.bw.coupon.vo.CouponTemplateSDK;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "coupon")
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    /** 关联优惠券模板的主键(逻辑外键) */
+    @Column(name = "template_id", nullable = false)
+    private Integer templateId;
+
+    /** 领取用户 */
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    /** 优惠券码 */
+    @Column(name = "coupon_code", nullable = false)
+    private String couponCode;
+
+    /** 领取时间 */
+    @CreatedDate
+    @Column(name = "assign_time", nullable = false)
+    private Date assignTime;
+
+    /** 优惠券状态 */
+    // @Basic //表示为这张表的某个列，是默认行为，与@Transient相对
+    @Column(name = "status", nullable = false)
+    @Convert(converter = CouponStatusEnumConverter.class)
+    private CouponStatusEnum status;
+
+    /** 用户优惠券对应的模板信息 */
+    @Transient  // 表示不为当前表的某个列，但是需要定义在这里，方便操作
+    private CouponTemplateSDK templateSDK;
+
+    /**
+     * 返回一个无效的 Coupon 对象
+     * */
+    public static Coupon invalidCoupon(){
+        Coupon coupon = new Coupon();
+        coupon.setId(-1);
+        return coupon;
+    }
+
+    /**
+     * 构造优惠券
+     **/
+    public Coupon(Integer templateId, Long userId, String couponCode,
+                  CouponStatusEnum status) {
+        this.templateId = templateId;
+        this.userId = userId;
+        this.couponCode = couponCode;
+        this.status = status;
+    }
+}
---
+++ coupon-service/coupon-distribution/src/main/java/com/bw/coupon/enumeration/CouponStatusEnum.java
@@ -0,0 +1,36 @@
+package com.bw.coupon.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * 用户优惠券的状态
+ */
+@AllArgsConstructor
+@Getter
+public enum  CouponStatusEnum {
+    USABLE("可用",1),
+    USED("已使用",2),
+    EXPIRED("已过期",3);
+
+    private String intro;
+    private Integer code;
+
+    /**
+     * 根据 code 获取到 CouponStatusEnum
+     * */
+    public static CouponStatusEnum of(Integer code) {
+
+        Objects.requireNonNull(code);
+
+        return Stream.of(values())
+                .filter(bean -> bean.code.equals(code))
+                .findAny()
+                .orElseThrow(
+                        () -> new IllegalArgumentException(code + " not exists")
+                );
+    }
+}
